2e07febe0bdfee4b66d7a4fc16a42e4dec2f27d6
matstars <matstars@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 16:03:01 +0000

    Updating trunk to 0.3.1
    git-svn-id: https://plugins.svn.wordpress.org/voce-seo/trunk@905680 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- voce-seo.php
+++ voce-seo.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: Voce SEO
-  Version: 0.3.0
+  Version: 0.3.1
   Plugin URI: http://voceconnect.com/
   Description: An SEO plugin taking things from both WP SEO and All in One SEO but leaving out the VIP incompatible pieces.
   Author: Voce Platforms
@@ -153,15 +153,16 @@ class VSEO {
 
 		$description    = self::get_meta_description();
 		$queried_object = get_queried_object();
-		if ( isset( $queried_object->post_type ) || ( is_tax() || is_category() || is_tag() ) ) {
+		if ( isset( $queried_object->post_type ) || ( is_tax() || is_category() || is_tag() ) || is_home() || is_front_page() ) {
 			$og_description = self::get_seo_meta( 'og_description', get_queried_object_id() );
-			if ( ! $og_description )
+			if ( ! $og_description ) {
 				$og_description = $description;
-			/* og_description is not required, so if it is not set, do not output it */
-						if ( $og_description ) $meta_objects = self::create_meta_object( 'og:description', 'meta', array( 'property' => 'og:description', 'content' => esc_attr( $og_description ) ), $meta_objects );
+			}
+			if ( $og_description ) {
+				$meta_objects = self::create_meta_object( 'og:description', 'meta', array( 'property' => 'og:description', 'content' => esc_attr( $og_description ) ), $meta_objects );
+			}
 		}
 		$meta_objects = self::create_meta_object( 'og:title', 'meta', array( 'property' => 'og:title', 'content' => esc_attr( self::get_social_title( 'og_title' ) ) ), $meta_objects );
-
 		$meta_objects = self::create_meta_object( 'og:type', 'meta', array( 'property' => 'og:type', 'content' => apply_filters( 'vseo_ogtype', 'article' ) ), $meta_objects );
 
 		if( $image = self::get_meta_image() ) {
@@ -254,6 +255,7 @@ class VSEO {
 	}
 
 	public static function get_social_title( $meta_key ) {
+		$title = '';
 		if ( is_home() || is_front_page() ) {
 			$title = get_bloginfo( 'name' );
 		} else if ( is_author() ) {
@@ -264,7 +266,9 @@ class VSEO {
 			$title = self::get_seo_meta( $meta_key );
 			if ( ! $title ) {
 				$vseo_meta = (array) get_post_meta( $post->ID, 'vseo_meta', true );;
-				$title = $vseo_meta['title'];
+				if ( array_key_exists( 'title', $vseo_meta ) ) {
+					$title = $vseo_meta['title'];
+				}
 				if ( ! $title ) {
 					$title = empty( $post->post_title ) ? ' ' : wp_kses( $post->post_title, array() ) ;
 				}
@@ -313,17 +317,17 @@ class VSEO {
 				$description = preg_replace( "/\s\s+/u", " ", $description );
 			}
 		} else {
-					if ( is_tax() || is_category() || is_tag() ) {
-							$queried_object = get_queried_object();  
-							$term_id = $queried_object->term_id; 
-							$taxonomy = $queried_object->taxonomy;
-							$description = term_description( $term_id, $taxonomy );
-							
-					} elseif ( is_search() ) {
-							$description = '';
-					} else {
-							$description = get_bloginfo( 'description', 'display' );
-					}
+			if ( is_tax() || is_category() || is_tag() ) {
+					$queried_object = get_queried_object();  
+					$term_id = $queried_object->term_id; 
+					$taxonomy = $queried_object->taxonomy;
+					$description = term_description( $term_id, $taxonomy );
+					
+			} elseif ( is_search() ) {
+					$description = '';
+			} else {
+					$description = get_bloginfo( 'description', 'display' );
+			}
 		}
 
 		$description = apply_filters( 'seo_meta_description', trim( $description ) );
@@ -438,7 +442,7 @@ class VSEO {
 
 		if ( isset($queried_object->post_type) ) {
 			if(  has_post_thumbnail( get_queried_object_id() )) {
-				$img = wp_get_attachment_image_src(get_post_thumbnail_id( get_queried_object_id() ), 'medium');
+				$img = wp_get_attachment_image_src(get_post_thumbnail_id( get_queried_object_id() ), apply_filters( 'vseo_image_size', 'full' ) );
 				if( !empty( $img[0] ) )
 					$img = $img[0];
 			}
